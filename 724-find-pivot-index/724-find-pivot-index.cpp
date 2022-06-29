cd8107607e10a6b6ea889f7c3cdd294d002a57bd
Manu Shekhar <86350276+iammanuss@users.noreply.github.com>
2022-06-29 14:17:18 +0530

    Time: 25 ms (89.05%), Space: 31.1 MB (87.15%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -3,16 +3,13 @@ public:
     int pivotIndex(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
-        // int sum=0;
-        // for(auto i:nums) sum+=i;
-        vector<int>pre(nums.size()),suf(nums.size());
-        pre[0]=nums[0];
-        for(int i=1;i<nums.size();i++)  pre[i]=pre[i-1]+nums[i];
-        suf[nums.size()-1]=nums[nums.size()-1];
-        for(int i=nums.size()-2;i>-1;i--) suf[i]=suf[i+1]+nums[i];
-        
+        int sum=0;
+        for(auto i:nums) sum+=i;
+        int suf=0,pre=0;
         for(int i=0;i<nums.size();i++){
-            if(pre[i] == suf[i])    return i;
+            suf=sum-pre;
+            pre+=nums[i];
+            if(suf==pre) return i;
         }
         return -1;
     }
